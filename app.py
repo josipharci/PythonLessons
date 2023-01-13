dda3abd3bebd16ee30c5d746a017a4eac6d5bb7c
Josip Harci <harcijosip99@gmail.com>
2023-01-13 23:16:14 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+print("Hello World!")
+
+#Drawnig a shape
+ 
+print("    /| ")
+print("   / | ")
+print("  /  | ")
+print(" /   | ")
+print("/____| ")
+
+#Variable & Data Types
+
+character_name = "Josip Harci"
+character_age = "23"
+character_like="programming"
+character_learning="python"
+
+print("I am " + character_name + ".")
+print("I am " + character_age + " years old. ")
+print("I like " + character_like +  ".")
+print("I'm learning " + character_learning + ".")
+
+
