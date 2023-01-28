78daabf709c075af2cb1128cee7cd1800124b098
Josip Harci <harcijosip99@gmail.com>
2023-01-28 11:00:14 +0100

    Update

--- lesson1.py
+++ lesson1.py
@@ -1,14 +1,14 @@
 #Prvi Code
 print("Hello World")
 
-# Zadatak 1.  Ispišite sumu (a + b + c)
-# i umnožak unesenih brojeva (a * b * c) ovisno o tome koja je od tih
-# dviju vrijednosti veća
+a = int(input("input num1: "))
+b = int(input("input num2: "))
 
-a = int(input("Enter your num a: "))
-b = int(input("Enter your num b: "))
-c = int(input("Enter your num c: "))
-sum1= a + b + c 
-sum2= a * b * c
-sum3= sum1 < sum2
-print(sum3)
+if a <  b:
+    x = b - a
+    y = bool(x)
+else:
+    x = a - b
+    y = bool(x)
+
+print(x , y)
