bdc0178024209f0e7a03e1fa409599ec5d33674c
Josip Harci <harcijosip99@gmail.com>
2023-01-28 11:29:37 +0100

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,13 @@
+# Zadatak 1:  Kreirajte dijagram toka koji učitava tri broja. Ispišite sumu (a + b + c) 
+# ili umnožak unesenih brojeva (a * b * c) ovisno o tome koja je od tih 
+# dviju vrijednosti veća.
+
+
+a=int(input("number1: "))
+b=int(input("number2: "))
+c=int(input("number3: "))
+
+sum1 = a + b + c 
+sum2 = a * b * c 
+
+     
