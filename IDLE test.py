c30ea0e905bcc33f6eedfcf394abb9b29cffb0d7
Josip Harci <harcijosip99@gmail.com>
2023-02-03 18:23:40 +0100

    Update IDLE test.py

--- IDLE test.py
+++ IDLE test.py
@@ -1,1 +1,20 @@
 print("Hello World")
+
+z = complex(1.11 , 2.22)
+print(z)
+print(z.real)
+print(z.imag)
+
+niz = 'Ovo je neki "Niz" znakova!'
+print(niz)
+
+niz = '''Prva Linija
+Druga Linija
+      Treča linija s razmakom'''
+print(niz)
+
+niz="Hello World"
+print(niz[0])
+print(niz[:5])
+print(niz[6:10])
+
