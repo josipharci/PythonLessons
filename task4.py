af5577ba8759021c1b7185a3aa1a1b79dc2501ae
Josip Harci <harcijosip99@gmail.com>
2023-02-03 20:40:21 +0100

    Update task4.py

---
+++ task4.py
@@ -0,0 +1,29 @@
+#Kreirajte dvije varijable i pridijelite im neke vrijednosti te uz pomoć 
+#aritmetičkih operatora (zbrajanje, oduzimanje, množenje, dijeljenje, 
+#ostatak dijeljenja, potenciranje, cjelobrojno dijeljenje) ispišite 
+#rezultate.
+
+num1 = int(input("Number1: "))
+num2 = int(input("Number2: "))
+
+x = num1 + num2
+print("num1 + num2 =", str(x))
+
+x = num1 * num2
+print("num1 * num2 =", str(x))
+
+x = num1 - num2
+print("num1 - num2 =", str(x))
+
+x = num1 / num2
+print("num1 / num2 =", str(x))
+
+x = num1 % num2
+print("num1 % num2 =", str(x))
+
+x = num1 ** num2
+print("num1 ** num2 =", str(x))
+
+x = num1 // num2
+print("num1 // num2 =", str(x))
+
