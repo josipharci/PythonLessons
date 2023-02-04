2047cd643141aca8746ecb1e8a96688b3a62e3d7
Josip Harci <harcijosip99@gmail.com>
2023-02-04 10:00:28 +0100

    Update task7.py

--- tasks/task7.py
+++ tasks/task7.py
@@ -1,6 +1,9 @@
-'''Kreirajte dvije brojčane varijable te ispišite rezultat operatora 
+'''
+Kreirajte dvije brojčane varijable te ispišite rezultat operatora 
 usporedbe (usporedba jednakosti, nejednakosti, strogo veće, strogo 
-manje, veće ili jednako, manje ili jednako)'''
+manje, veće ili jednako, manje ili jednako)
+
+'''
 
 num1 = 10
 num2 = 15
@@ -22,3 +25,23 @@ print(num1 >= num2)
 
 # manje ili jednako
 print(num1 <= num2)
+
+'''
+Kreirajte dvije varijable i pridijelite im vrijednosti True ili False. 
+Ispišite rezultate logičkih operatora (and, or, not) za dvije 
+prethodno kreirane varijable
+
+'''
+
+var1 = False
+var2 = True
+
+# And
+print(var1 and var2 , "AND")
+
+# Or
+print(var1 or var2 , "OR")
+
+# Not
+print(not var1 , "NOT") 
+print(not var2 , "NOT")
