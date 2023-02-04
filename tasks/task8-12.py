b195b16a70b003287d0e38636cb0d3a6f779ae7f
Josip Harci <harcijosip99@gmail.com>
2023-02-04 19:19:12 +0100

    Update task8-12.py

--- tasks/task8-12.py
+++ tasks/task8-12.py
@@ -6,3 +6,41 @@ sljedećem zadatku. Aritmetička sredina računa se tako da se sve
 dane vrijednosti zbroje te se rezultat podijeli s brojem varijabli
 
 '''
+a = 5
+b = 10
+c = 15
+d = 21
+
+x = a + b + c + d / 4
+
+# print(x)
+
+'''
+
+iskoristite prethodni zadatak te dobiveni rezultat pretvorite u cijeli broj
+i tu vrijednost spremite u varijablu. Nad cjelobrojnom aritmetičkom 
+sredinom koju ste prethodno spremili u varijablu napravite
+kvadriranje i ispišite dobiveni rezultat
+
+'''
+y = int(x) * int(x)
+
+# print(y)
+
+'''
+Korištenjem skraćenih aritmetičkih operatora rezultat dobiven iz 
+prethodnog zadatka pomnožite sa 100 i ispišite dobivenu vrijednost.
+
+'''
+y*=100
+
+# print(y)
+
+'''
+Pomoću operatora usporedbe provjerite je li broj koji ste dobili u 
+prethodnom zadatku manji od 500. Ispišite rezultat usporedbe (ispis 
+će biti vrijednosti "True" ili "False").
+
+'''
+
+print(y < 500)
