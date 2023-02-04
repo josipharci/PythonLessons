5ee4a2fa5b12322af5f3feb8077453b978f6f470
Josip Harci <harcijosip99@gmail.com>
2023-02-04 11:55:21 +0100

    Update lesson3.1.py

--- lesson3.1.py
+++ lesson3.1.py
@@ -1,1 +1,20 @@
 # NAREDBA break i continue
+
+# Break
+for n in range(2,11):
+    isPri = True
+    for x in range(2 , n):
+        if n % x == 0:
+            isPri = False
+            break
+        if isPri == True:
+            print('Number', n , 'is prime!')
+        else:
+            print('Number', n , 'is not prime!')
+
+# Continue
+for num in range(2,6):
+    if num % 2 == 0:
+        print("Even number:", num)
+        continue
+    print("Odd number:", num)
