af04d3570f0fda09789e6fdfbf2fb50c1291874b
Josip Harci <harcijosip99@gmail.com>
2023-02-04 09:51:03 +0100

    Create task7.py

---
+++ tasks/task7.py
@@ -0,0 +1,24 @@
+'''Kreirajte dvije brojčane varijable te ispišite rezultat operatora 
+usporedbe (usporedba jednakosti, nejednakosti, strogo veće, strogo 
+manje, veće ili jednako, manje ili jednako)'''
+
+num1 = 10
+num2 = 15
+
+# jednakost 
+print(num1 == num2)
+
+# nejdednakost 
+print(num1 != num2)
+
+# strogo veče 
+print(num1 > num2)
+
+# strogo manje 
+print(num1 < num2)
+
+# veće ili jednako
+print(num1 >= num2)
+
+# manje ili jednako
+print(num1 <= num2)
