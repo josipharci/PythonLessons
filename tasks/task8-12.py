c215260bd77b09c3428ee18f6b9dfc17be0cdf8f
Josip Harci <harcijosip99@gmail.com>
2023-02-04 10:22:46 +0100

    Create task8-12.py

---
+++ tasks/task8-12.py
@@ -0,0 +1,8 @@
+'''
+Napišite program u kojem su navedene varijable a=5, b=10, 
+c=15, d=21. Program mora vratiti aritmetičku sredinu danih 
+brojeva. Rezultat spremite u varijablu jer će se vrijednost koristiti i u 
+sljedećem zadatku. Aritmetička sredina računa se tako da se sve 
+dane vrijednosti zbroje te se rezultat podijeli s brojem varijabli
+
+'''
