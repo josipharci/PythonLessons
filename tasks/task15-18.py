992025a5ff06eb9014256802fd2e9e8110142a57
Josip Harci <harcijosip99@gmail.com>
2023-02-05 16:54:46 +0100

    Update task15-18.py

--- tasks/task15-18.py
+++ tasks/task15-18.py
@@ -21,3 +21,49 @@ print("oct b:",oct(y))
 x = int(c)
 print("hex c:",hex(x))
 print("oct c:",oct(x))
+
+'''
+Kreirajte varijablu te u nju spremite neki niz znakova. Tu varijablu 
+upotrijebite kako biste isprobali korištenje funkcija i metoda za rad s 
+nizovima znakova (capitalize(), len(), lower(), upper(), 
+title(), lstrip(), rstrip(), strip()).
+
+'''
+
+text= " Hello World  "
+print(text.capitalize())
+print(len(text))
+print(text.lower())
+print(text.upper())
+print(text.title())
+print(text.lstrip())
+print(text.rstrip())
+print(text.strip())
+
+'''
+Dodatno:  U varijablu upišite neki proizvoljni niz znakova. Nad varijablom 
+pozovite odgovarajuću funkciju koja će vratiti duljinu upisanoga niza 
+znakova te rezultat spremite u varijablu. Na temelju duljine niza 
+ispišite sve znakove do polovice niza. Primjer: ako imamo niz od 14
+znakova (abcdefghijklmn), potrebno je ispisati 1., 2., 3., 4., 5., 6. i 7. 
+znak (abcdefg).
+
+'''
+
+
+
+
+
+
+
+'''
+Dodatno:  Napišite program koji će u varijablu spremiti neku vrijednost 
+temperature (izražene u stupnjevima Celzijevim). Na ekran ispisati 
+vrijednost temperature u farenhajtima. Formula: ( 𝑥 ×
+9
+5
+) + 32 , x je 
+vrijednost izražena u stupnjevima Celzijevim.
+
+'''
+
