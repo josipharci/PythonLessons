6b4ee5526c9572f9c3e634f4c61752abe8a6b4c5
Josip Harci <harcijosip99@gmail.com>
2023-02-05 16:41:24 +0100

    Create task15-18.py

---
+++ tasks/task15-18.py
@@ -0,0 +1,23 @@
+'''
+Kreirajte tri brojčane varijable te pomoću njih isprobajte funkcije za 
+rad s brojevima (int(), float(), complex(), hex(), oct()).
+
+'''
+
+a = 15
+b = 10.5
+c = 5.23
+
+print("int a:",int(a),"int b:",int(b),"int c:",int(c))
+print("float a:",float(a),"float b:",float(b),"float c:",float(c))
+print("complex a:",complex(a),"complex b:",complex(b),"complex c:",complex(c))
+print("hex a:",hex(a))
+print("oct a:",oct(a))
+
+y = int(b)
+print("hex b:",hex(y))
+print("oct b:",oct(y))
+
+x = int(c)
+print("hex c:",hex(x))
+print("oct c:",oct(x))
