79551b2a1731f13087c59a5a420a7a80ecf06df2
Josip Harci <harcijosip99@gmail.com>
2023-02-06 09:49:04 +0100

    Create lesson4.py

---
+++ lesson4.py
@@ -0,0 +1,39 @@
+# Funkcije
+
+def sumNumber():
+    print("Hello World")
+
+sumNumber()
+
+
+# Bitno
+def squ(x):
+    print(x * x)
+
+squ(int(input("num:")))
+
+def sum(var1,var2,var3):
+    print( var1 + var2 + var3)
+
+sum(10,15,20)
+
+
+def squ(x):
+    return x * x
+
+var1 = squ(2)
+var2 = squ(5)
+
+print(var1 + var2)
+
+def test():
+    print(var)
+
+var=5
+test()
+
+
+
+
+
+
