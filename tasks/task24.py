191a2d9507936a063c5d408231f269f08da24a56
Josip Harci <harcijosip99@gmail.com>
2023-02-07 10:30:41 +0100

    Create task24.py

---
+++ tasks/task24.py
@@ -0,0 +1,24 @@
+'''
+U varijable a1, a2, b1, b2 spremite četiri cijela broja. Neka 
+vrijednosti koje spremite u varijable zadovoljavaju sljedeće uvjete: a1
+< a2 i b1 < b2. Te vrijednosti predstavljaju granice dvaju intervala 
+[a1, a2] – prvi interval i [b1, b2] – drugi interval. Provjerite je li drugi 
+interval smješten unutar prvog intervala.
+Napomena: provjerite je li početna granica prvog intervala manja ili 
+jednaka početnoj granici drugog intervala (a1 ≤ b1) te je li završna 
+granica prvog intervala veća ili jednaka završnoj granici drugog 
+intervala (b2 ≤ a2). 
+Ako su ovi uvjeti zadovoljeni, ispišite poruku "Zadovoljava.", u 
+suprotnom ispišite poruku "Ne zadovoljava.".
+
+'''
+a1 = int(4)
+a2 = int(8)
+
+b1 = int(5)
+b2 = int(7)
+
+if a1 <= b1 and a2 >= b2:
+    print("Zadovoljava")
+else: 
+    print("Ne zadovoljava")
