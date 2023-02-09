22fb18eabd5b7cf18cc3c1700d0b42ab5ca85de1
Josip Harci <harcijosip99@gmail.com>
2023-02-09 17:39:58 +0100

    Create task28.py

---
+++ tasks/Lesson4.task/task28.py
@@ -0,0 +1,31 @@
+'''
+Napišite funkciju koja prima 2 parametara. Rezultat izračuna funkcije 
+ovog puta se ne ispisuje izravno u funkciji nego u glavnom dijelu 
+programa. Funkcija mora izračunati rezultat formule:
+(a*a) + (b*b)
+Rezultat spremite u varijablu koja se nalazi u dijelu programskoga
+kôda u kojem se funkcija poziva te ispišite tu varijablu.
+
+'''
+
+def fun(a, b):
+    return (a*a) + (b*b)
+
+var1 = fun(5,7)
+print (var1)
+
+'''
+Pozovite funkciju koja ne prima nijedan parametar, ali mora 
+izračunati i ispisati zbroj dvaju brojeva. Brojevi neka se dohvate iz 
+glavnog dijela programa preko globalnih varijabli.
+
+'''
+
+def sum():
+   return a + b
+
+a = 5
+b = 9
+
+res = sum()
+print(res)
