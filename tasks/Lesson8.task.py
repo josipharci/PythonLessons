e29697f6ac61bf55e6e944b7eba7e339dbfcaf3d
Josip Harci <harcijosip99@gmail.com>
2023-02-10 15:41:56 +0100

    Create Lesson8.task.py

---
+++ tasks/Lesson8.task.py
@@ -0,0 +1,31 @@
+'''
+Napišite skriptu koja od korisnika zahtjeva unos dvaju brojeva, ta
+skripta neka ispisuje njihov zbroj. Tako napisanu skriptu pokrenuti na 
+oba načina pokretanja skripti opisana u ovom poglavlju. Što 
+primjećujete kao razliku između načina pokretanja skripte dvostrukim 
+klikom na nju i načina pokretanja skripte preko naredbenog retka?
+
+'''
+
+a = input("Unesite prvi broj: ")
+b = input("Unesite drugi broj: ")
+rez = int(a) + int(b)
+print(rez)
+
+
+'''
+Kreirajte zadatak koji će se samostalno izvršavati svaku minutu. 
+Skripta koju će kreirani zadatak izvršiti neka ispiše prilikom svakog 
+izvršavanja u neku proizvoljnu datoteku vrijednost "x". Obratite 
+pozornost da se sadržaj datoteke ne smije brisati, već se mora 
+prilikom svakog izvršavanja u datoteku dopisati jedan "x".
+
+'''
+
+
+
+
+
+
+
+
