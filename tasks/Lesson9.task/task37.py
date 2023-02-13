30b88678c1fa534e3af492115eb36783eca57d99
Josip Harci <harcijosip99@gmail.com>
2023-02-13 17:31:30 +0100

    Update task37.py

--- tasks/Lesson9.task/task37.py
+++ tasks/Lesson9.task/task37.py
@@ -4,8 +4,18 @@ istina ako su svi elementi liste parni brojevi, a boolean vrijednost laž
 ako postoji barem jedan broj koji nije paran.
 
 '''
+def  fun(a):
+    for i in a:
+        if i%2 == 1:
+            return False
+    return True
 
+a = [2, 4, 6, 8, 9]
 
+if fun(a) == True:
+    print("Svi su elementi parni!")
+else:
+    print("Postoji barem jedan neparan broj!")
 
 
 
@@ -20,3 +30,18 @@ lista učitana, program mora zamijeniti prvi element s posljednjim,
 drugi element s pretposljednjim i tako redom.
 
 '''
+
+array = []
+
+while True:
+    num = input("Unesite broj: ")
+    num = int(num)
+
+    if num == 5:
+        break
+    else:
+        array.append(num)
+
+print(array)
+array.reverse()
+print(array)
