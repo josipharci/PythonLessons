47a37161ed0c98f957e983bb548e47eeedd6c4ae
Josip Harci <harcijosip99@gmail.com>
2023-03-07 19:25:57 +0100

    Create buttons.py

---
+++ PythonGUI/buttons.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+
+def myClick():
+    myLabel = Label(root, text="Look! I clicked a Button!")
+    myLabel.pack()
+
+myButton = Button(root,text="Click Me!",padx=50,pady=50, command=myClick)
+myButton.pack()
+
+
+root.mainloop()
+
+
+
