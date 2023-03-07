90c42d9f8f66c2a39dc2b61100fe44c63b9229dc
Josip Harci <harcijosip99@gmail.com>
2023-03-07 19:16:04 +0100

    Update grid.py

--- PythonGUI/grid.py
+++ PythonGUI/grid.py
@@ -2,9 +2,14 @@ from tkinter import *
 
 root = Tk()
 
-myLabel = Label(root, text="Hello World!")
+myLabelOne = Label(root, text="Hello World!")
+myLabelTwo = Label(root, text="My name is Josip Harci")
+myLabelThree = Label(root, text="                         ")
 
-myLabel.pack()
+myLabelOne.grid(row=0, column=0)
+myLabelTwo.grid(row=1, column=5)
+myLabelThree.grid(row=1, column=1)
+
 
 root.mainloop()
 
