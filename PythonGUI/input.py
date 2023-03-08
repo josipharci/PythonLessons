97a4451a2f2f6f0d2c0492ab6716a66b295873e7
Josip Harci <harcijosip99@gmail.com>
2023-03-08 17:59:43 +0100

    Update

--- PythonGUI/input.py
+++ PythonGUI/input.py
@@ -2,11 +2,12 @@ from tkinter import *
 
 root = Tk()
 
+
 def myClick():
     myLabel = Label(root, text="Look! I clicked a Button!")
     myLabel.pack()
 
-myButton = Button(root,text="Click Me!",padx=20,pady=10, command=myClick ,fg="blue" , bg="red")
+myButton = Button(root,text="Click Me!", command=myClick)
 myButton.pack()
 
 
