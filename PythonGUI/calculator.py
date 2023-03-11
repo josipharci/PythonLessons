6a15c3e32800a4bebd3b602b22f1aebb6351cb36
Josip Harci <harcijosip99@gmail.com>
2023-03-11 20:27:14 +0100

    Update calculator.py

--- PythonGUI/calculator.py
+++ PythonGUI/calculator.py
@@ -3,17 +3,26 @@ from tkinter import *
 root = Tk()
 root.title("Simple Calculator")
 
-e = Entry(root, width=35,borderwidth=5)
-e.grid(row=0,column=0,columnspan=3,padx=10,pady=10)
+e = Entry(root, width=35, borderwidth=5)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-e.insert(0,"Enter Your Name:")
+#e.insert(0, "")
 
-def myClick():
-    hello = "Hello" + e.get()
-    myLabel = Label(root,text=hello)
-    myLabel.pack()
+def button_add():
+    return
 
-myButton = Button(root, text="Enter Your Stock Quote"  , command=myClick)
+buttonOne = Button(root, text="1", padx=40, pady=20, command=button_add)
+buttonTwo = Button(root, text="2", padx=40, pady=20, command=button_add)
+buttonThree = Button(root, text="3", padx=40, pady=20, command=button_add)
+buttonFour = Button(root, text="4", padx=40, pady=20, command=button_add)
+buttonFive = Button(root, text="5", padx=40, pady=20, command=button_add)
+buttonSix = Button(root, text="6", padx=40, pady=20, command=button_add)
+buttonSeven = Button(root, text="7", padx=40, pady=20, command=button_add)
+buttonEight = Button(root, text="8", padx=40, pady=20, command=button_add)
+buttonNine = Button(root, text="9", padx=40, pady=20, command=button_add)
+buttonZero = Button(root, text="0", padx=40, pady=20, command=button_add)
+
+myButton = Button(root,  text="Enter Your Stock Quote",  command=myClick)
 
 
 root.mainloop()
